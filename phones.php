2768ccb947753a151ee77c67e2f72896efa8af06
Start Coding Student 2 <session2@START-Coding-MBP-09.local>
2016-02-12 17:42:40 -0700

    made a faqs html and changed the phones

--- phones.php
+++ phones.php
@@ -119,12 +119,7 @@ catch(PDOException $e)
         </th>
     </tr>
 </table>
-<<div>
-    <a href="/SimpleCart">Home</a>
-    <a href="/SimpleCart/login">Login</a>
-    <a href="/SimpleCart/register">Register</a>
-    <a href="/SimpleCart/cart">Cart</a>
-</div><br><br>
+<br><br>
 <form method="post" action="">
     <input type="text" name="username" placeholder="Username"/>
     <input type="password" name="password" placeholder="Password"/>
